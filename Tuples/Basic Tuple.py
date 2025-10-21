83ae9c038839ba01ad66039d68a7e61ba3d663fa
mdshihabcn72 <mdshihab726319@gmail.com>
2025-10-21 18:53:35 +0000

    Practice Tuple

---
+++ Tuples/Basic Tuple.py
@@ -0,0 +1,8 @@
+# A simple tuple of numbers and demonstration of indexing.
+nums = (1,2,3,4,5,6)
+
+print("Tuple : ",nums)
+print("First Element :", nums[0])
+print("Last Element : ", nums[-1])
+print("Slice (2:5) : ", nums[2:5])
+print("Length of Tuple : ", len(nums))
